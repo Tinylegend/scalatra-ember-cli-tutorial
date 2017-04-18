b5f8532a9b9191bd6ac4f5f5889b440be08dc88c
Rocky Fan <jufan@expedia.com>
2017-04-17 22:26:41 -0700

    Initialize the Scalatra project form RHome/scalatra-sbt.g8:  https://github.com/RHome/scalatra-sbt.g8.

---
+++ tiny-legend-web/src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,23 @@
+import com.tinylegend.app._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+import org.scalatra.metrics.MetricsBootstrap
+import org.scalatra.metrics.MetricsSupportExtensions._
+
+class ScalatraBootstrap extends LifeCycle with MetricsBootstrap {
+
+  //override val metricRegistry = ???
+  //override val healthCheckRegistry = ???
+
+  override def init(context: ServletContext) {
+
+    context.mountMetricsAdminServlet("/metrics-admin")
+    context.mountHealthCheckServlet("/health")
+    context.mountMetricsServlet("/metrics")
+    context.mountThreadDumpServlet("/thread-dump")
+    context.installInstrumentedFilter("/test/*")
+
+    context.mount(new LegendServlet, "/*")
+  }
+}
