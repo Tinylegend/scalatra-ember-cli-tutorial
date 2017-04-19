e14edc6ee658fc1f4f2cafe2104fb15b83f4ef16
Rocky Fan <fanrocky@gmail.com>
2017-04-19 00:09:56 -0700

    Update the ember configure the locationType to has instead of auto. When in ‘auto’, the URL path would be something like ‘/user/login’. When in ‘hash’, the URL path would be something like ‘/user/#/login’.
    In first ‘auto’ model, server container will treat it as sub folder hence it would not have folder /login under /user so we will get 404 when doing refresh from page.
    When doing hash, server container will look at ‘/user’, then use ‘#/login’ as mark in the page. In /user, we have index.html which is severed as default page. So we can find the page.

--- ember-admin/config/environment.js
+++ ember-admin/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-admin',
     environment: environment,
     rootURL: '/admin',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
